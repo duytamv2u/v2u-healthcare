7bf5a0b3da0f615e09766308b7295b8e471efb5b
Thai Duy Tâm <thaisam.duy@gmail.com>
2025-04-27 11:01:17 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-// pages/index.js
+/ pages/index.js
 
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
@@ -13,13 +13,15 @@ export default function Home() {
       </Head>
       
       <main className={styles.main}>
-        {/* Sử dụng class local */}
+        {/* Áp dụng class local */}
         <h1 className={styles.title}>Chào mừng bạn đến với V2U Health Care</h1>
         <h2 className={styles.subtitle}>Hãy tham khảo các sản phẩm của chúng tôi.</h2>
         <p>
           Hãy nhấn vào nút dưới đây để được hướng dẫn chi tiết.
         </p>
-        <button onClick={() => alert('Chatbot đang mở')}>Bấm vào đây để được hướng dẫn chi tiết</button>
+        <button className={styles.button} onClick={() => alert('Chatbot đang mở')}>
+          Bấm vào đây để được hướng dẫn chi tiết
+        </button>
       </main>
 
       <footer className={styles.footer}>
