e9fac3a4c7972bb98368e3a93d83fa25bfdc8773
Thai Duy Tâm <thaisam.duy@gmail.com>
2025-04-27 10:11:44 +0700

    Create Chatbot.js

---
+++ components/Chatbot.js
@@ -0,0 +1,12 @@
+export default function Chatbot() {
+  return (
+    <div style={{ width: '100%', height: '700px' }}>
+      <iframe
+        src="https://www.chatbase.co/chatbot-iframe/Fm0rsLXH6sQDWKkx3BVjM"
+        width="100%"
+        style={{ height: '100%', border: 'none' }}
+        frameBorder="0"
+      ></iframe>
+    </div>
+  );
+}
